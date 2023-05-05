861e79694aefd0351e6d90ea662043038ae6a0a7
David Waroquiers <david.waroquiers@matgenix.com>
2023-05-05 17:38:09 +0200

    Fixed unit test.

--- tests/io/test_slurm.py
+++ tests/io/test_slurm.py
@@ -3,10 +3,10 @@ from pathlib import Path
 import pytest
 from monty.serialization import loadfn
 
-from qtoolkit.queue.slurm import SlurmQueue
+from qtoolkit.io.slurm import SlurmIO
 
 TEST_DIR = Path(__file__).resolve().parents[1] / "test_data"
-ref_file = TEST_DIR / "queue" / "slurm" / "parse_submit_cmd_inout.yaml"
+ref_file = TEST_DIR / "io" / "slurm" / "parse_submit_cmd_inout.yaml"
 in_out_ref_list = loadfn(ref_file)
 
 
@@ -15,6 +15,6 @@ def test_parse_submit_cmd_output(in_out_ref, test_utils):
     parse_cmd_output, sr_ref = test_utils.inkwargs_outref(
         in_out_ref, inkey="parse_submit_kwargs", outkey="submission_result_ref"
     )
-    slurm_q = SlurmQueue()
-    sr = slurm_q._parse_submit_cmd_output(**parse_cmd_output)
+    slurm_io = SlurmIO()
+    sr = slurm_io.parse_submit_output(**parse_cmd_output)
     assert sr == sr_ref
