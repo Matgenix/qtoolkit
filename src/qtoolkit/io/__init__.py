c2791e6e1121e4cc7906e13cbf3a8970775560f3
Guido Petretto <g.petretto@gmail.com>
2023-06-28 13:55:04 +0200

    update shell io

--- src/qtoolkit/io/__init__.py
+++ src/qtoolkit/io/__init__.py
@@ -3,4 +3,4 @@ from qtoolkit.io.pbs import PBSIO, PBSState
 from qtoolkit.io.shell import ShellIO, ShellState
 from qtoolkit.io.slurm import SlurmIO, SlurmState
 
-scheduler_mapping = {"slurm": SlurmIO, "pbs": PBSIO}
+scheduler_mapping = {"slurm": SlurmIO, "pbs": PBSIO, "shell": ShellIO}
