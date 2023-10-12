2fac00b431952f54d5c3d6531bed8d1f7aff0f4e
David Waroquiers <david.waroquiers@matgenix.com>
2023-10-12 08:57:19 +0200

    Refactored tests for BaseScheduler into a class.

--- tests/io/test_base.py
+++ tests/io/test_base.py
@@ -51,74 +51,83 @@ def test_qtemplate():
     )
 
 
-def test_base_scheduler():
-    class MyScheduler(BaseSchedulerIO):
-        pass
-
-    with pytest.raises(TypeError):
-        MyScheduler()
-
-    class MyScheduler(BaseSchedulerIO):
-        header_template = """#SPECCMD --option1=$${option1}
+class TestBaseScheduler:
+    @pytest.fixture(scope="module")
+    def scheduler(self):
+        class MyScheduler(BaseSchedulerIO):
+            header_template = """#SPECCMD --option1=$${option1}
 #SPECCMD --option2=$${option2}
 #SPECCMD --option3=$${option3}"""
 
-        SUBMIT_CMD = "mysubmit"
-        CANCEL_CMD = "mycancel"
+            SUBMIT_CMD = "mysubmit"
+            CANCEL_CMD = "mycancel"
 
-        def parse_submit_output(self, exit_code, stdout, stderr) -> SubmissionResult:
+            def parse_submit_output(
+                self, exit_code, stdout, stderr
+            ) -> SubmissionResult:
+                pass
+
+            def parse_cancel_output(self, exit_code, stdout, stderr) -> CancelResult:
+                pass
+
+            def _get_job_cmd(self, job_id: str) -> str:
+                pass
+
+            def parse_job_output(self, exit_code, stdout, stderr) -> QJob | None:
+                pass
+
+            def _convert_qresources(self, resources: QResources) -> dict:
+                pass
+
+            @property
+            def supported_qresources_keys(self) -> list:
+                return []
+
+            def _get_jobs_list_cmd(
+                self, job_ids: list[str] | None = None, user: str | None = None
+            ) -> str:
+                pass
+
+            def parse_jobs_list_output(self, exit_code, stdout, stderr) -> list[QJob]:
+                pass
+
+        return MyScheduler()
+
+    def test_subclass_base_scheduler(self, scheduler):
+        class MyScheduler(BaseSchedulerIO):
             pass
 
-        def parse_cancel_output(self, exit_code, stdout, stderr) -> CancelResult:
-            pass
+        with pytest.raises(TypeError):
+            MyScheduler()
 
-        def _get_job_cmd(self, job_id: str) -> str:
-            pass
+    def test_generate_header(self, scheduler):
+        header = scheduler.generate_header({"option2": "value_option2"})
+        assert header == """#SPECCMD --option2=value_option2"""
 
-        def parse_job_output(self, exit_code, stdout, stderr) -> QJob | None:
-            pass
+    def test_generate_ids_list(self, scheduler):
+        ids_list = scheduler.generate_ids_list(
+            [QJob(job_id=4), QJob(job_id="job_id_abc1"), 215, "job12345"]
+        )
+        assert ids_list == ["4", "job_id_abc1", "215", "job12345"]
 
-        def _convert_qresources(self, resources: QResources) -> dict:
-            pass
+    def test_get_submit_cmd(self, scheduler):
+        submit_cmd = scheduler.get_submit_cmd()
+        assert submit_cmd == "mysubmit submit.script"
+        submit_cmd = scheduler.get_submit_cmd(script_file="sub.sh")
+        assert submit_cmd == "mysubmit sub.sh"
 
-        @property
-        def supported_qresources_keys(self) -> list:
-            return []
+    def test_get_cancel_cmd(self, scheduler):
+        cancel_cmd = scheduler.get_cancel_cmd(QJob(job_id=5))
+        assert cancel_cmd == "mycancel 5"
+        cancel_cmd = scheduler.get_cancel_cmd(QJob(job_id="abc1"))
+        assert cancel_cmd == "mycancel abc1"
+        cancel_cmd = scheduler.get_cancel_cmd("jobid2")
+        assert cancel_cmd == "mycancel jobid2"
+        cancel_cmd = scheduler.get_cancel_cmd(632)
+        assert cancel_cmd == "mycancel 632"
 
-        def _get_jobs_list_cmd(
-            self, job_ids: list[str] | None = None, user: str | None = None
-        ) -> str:
-            pass
-
-        def parse_jobs_list_output(self, exit_code, stdout, stderr) -> list[QJob]:
-            pass
-
-    scheduler = MyScheduler()
-
-    header = scheduler.generate_header({"option2": "value_option2"})
-    assert header == """#SPECCMD --option2=value_option2"""
-
-    ids_list = scheduler.generate_ids_list(
-        [QJob(job_id=4), QJob(job_id="job_id_abc1"), 215, "job12345"]
-    )
-    assert ids_list == ["4", "job_id_abc1", "215", "job12345"]
-
-    submit_cmd = scheduler.get_submit_cmd()
-    assert submit_cmd == "mysubmit submit.script"
-    submit_cmd = scheduler.get_submit_cmd(script_file="sub.sh")
-    assert submit_cmd == "mysubmit sub.sh"
-
-    cancel_cmd = scheduler.get_cancel_cmd(QJob(job_id=5))
-    assert cancel_cmd == "mycancel 5"
-    cancel_cmd = scheduler.get_cancel_cmd(QJob(job_id="abc1"))
-    assert cancel_cmd == "mycancel abc1"
-    cancel_cmd = scheduler.get_cancel_cmd("jobid2")
-    assert cancel_cmd == "mycancel jobid2"
-    cancel_cmd = scheduler.get_cancel_cmd(632)
-    assert cancel_cmd == "mycancel 632"
-
-    with pytest.raises(
-        ValueError,
-        match=r"The id of the job to be cancelled should be defined. Received: None",
-    ):
-        scheduler.get_cancel_cmd(job=None)
+        with pytest.raises(
+            ValueError,
+            match=r"The id of the job to be cancelled should be defined. Received: None",
+        ):
+            scheduler.get_cancel_cmd(job=None)
